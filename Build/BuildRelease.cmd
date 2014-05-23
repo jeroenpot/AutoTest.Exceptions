d925ae277fed6b3a385b03362a5d7a10bec186fe
Jeroen Pot <info@jeroenpot.nl>
2014-05-23 13:51:56 +0200

    Add simple compile and nuget pack scripts

---
+++ Build/BuildRelease.cmd
@@ -0,0 +1,2 @@
+set msbuildemitsolution=1
+msbuild.exe ..\Source\AutoTest.Exceptions\AutoTest.Exceptions.sln /t:Build /p:Configuration=Release
--- Source/AutoTest.Exceptions/AutoTest.Exceptions/Properties/AssemblyInfo.cs
+++ Source/AutoTest.Exceptions/AutoTest.Exceptions/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AutoTest.Exceptions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Automaticly test all your custom exceptions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompanyAttribute("Jeroen Pot")]
 [assembly: AssemblyProduct("AutoTest.Exceptions")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Jeroen Pot 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
